443a7fb4bc6f41beb3c0cf024092a57d94e17d34
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2024-04-15 18:16:18 -0600

    Navbar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,18 +2,21 @@ import { Routes, Route  } from 'react-router-dom';
 import Account from './pages/Account';
 import Home from './pages/Home';
 import './App.css'
+import Navbar from './components/navbar';
 
 function App() {
 
   return (
-
-  <div className="cont">
-    <Routes>
-      <Route path="/" element={< Home />}/>
-      <Route path="/create" element={< Account />} />
-    </Routes>
-  </div>
-  )
+    <div className="App">
+      <Navbar />
+      <div className="cont">
+        <Routes>
+          <Route path="/" element={< Home />}/>
+          <Route path="/create" element={< Account />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App
---
+++ front/src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './navbar.css';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="left-side">
+                <div className="navbar-links">
+                    <Link className="navbar-link" to="/">Home</Link>
+                    <Link className="navbar-link" to="/create">CRUD</Link>
+                    <Link className="navbar-link" to="/fraud-detection">Fraud Detection</Link>
+                </div>
+            </div>
+            <div className="right-side">
+                <img src={logo} alt="Logo" className="navbar-logo" />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
