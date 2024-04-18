082f8e68a0dd7497b1840a0c6cedfdbc516d63fa
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2024-04-17 19:32:41 -0600

    Doit Home

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import React from 'react';
+import './Home.css'; // Asegúrate de que el CSS se está cargando correctamente
 
 const Home = () => {
-return (
-    <div>
-      <h1>Home</h1>
+  return (
+    <div className='home-container'>
+      <div className="title-container">
+        <h1>Bienvenido al sistema de seguridad bancaria AFD</h1>
+      </div>
+      <div className="view">
+        <div className="plane main">
+          {Array.from({ length: 5 }, (_, i) => (
+            <div key={i} className="circle" style={{ transform: `rotateZ(${i / 5 * 360}deg) rotateX(63.435deg)` }} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
